7fe5c5740b9cc8cb2c73365c53336bfe9c0996e3
kenhanscombe <ken.hanscombe@gmail.com>
2017-02-22 14:34:37 +0000

    rm objects in .GlobalEnv

--- R/ukb_dataset.R
+++ R/ukb_dataset.R
@@ -33,8 +33,9 @@ ukb_df <- function(fileset, path = ".", data.pos = 2) {
   )
 
   variable_names <- .column_name_lookup(tables[[data.pos]])
-  names(bd) <- variable_names[names(bd)]
+
   df <- bd
+  names(df) <- variable_names[names(df)]
   rm(list = ls(pattern = "^bd$|^lbl.*|^lvl.*"), envir = .GlobalEnv)
   return(df)
 }

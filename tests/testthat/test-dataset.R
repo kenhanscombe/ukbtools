fbc13cf1a57434f6fe55d0cccdf511a8eb25e662
kenhanscombe <ken.hanscombe@gmail.com>
2021-10-20 13:43:05 +0100

    Test ukb_df returns dataframe

--- tests/testthat/test-dataset.R
+++ tests/testthat/test-dataset.R
@@ -10,3 +10,10 @@ test_that("ukb_df_field() creates a valid field-lookup dataframe", {
     expect_true(all(complete.cases(f)))
     expect_false(all(grepl("[[:punct:]]", f[["col.name"]])))
 })
+
+test_that("ukb_df() creates a dataframe", {
+    path_to_example_data <- system.file("extdata", package = "ukbtools")
+    df <- ukb_df("ukbxxxx", path_to_example_data)
+
+    expect_s3_class(df, "data.frame")
+})
